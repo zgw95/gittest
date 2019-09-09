6e8a3b1b0165242a61cf7af061f5613cc6fb6076
Zhang.Guo-Wang 張國旺 ITC <ITC190022@itc.inventec>
2019-09-09 10:10:39 +0800

    init commit

---
+++ 123.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace OperatorOvlApplication
+{
+    class Box
+    {
+        private double length;      // 长度
+        private double breadth;     // 宽度
+        private double height;      // 高度
+
+        public double getVolume()
+        {
+            return length * breadth * height;
+        }
+        public void setLength(double len)
+        {
+            length = len;
+        }
+
+        public void setBreadth(double bre)
+        {
+            breadth = bre;
+        }
+
+        public void setHeight(double hei)
+        {
+            height = hei;
+        }
+        // 重载 + 运算符来把两个 Box 对象相加
+        public static Box operator +(Box b, Box c)
+        {
+            Box box = new Box();
+            box.length = b.length + c.length;
+            box.breadth = b.breadth + c.breadth;
+            box.height = b.height + c.height;
+            return box;
+        }
+
+    }
+
+    class Tester
+    {
+        static void Main(string[] args)
+        {
+            Box Box1 = new Box();         // 声明 Box1，类型为 Box
+            Box Box2 = new Box();         // 声明 Box2，类型为 Box
+            Box Box3 = new Box();         // 声明 Box3，类型为 Box
+            double volume = 0.0;          // 体积
+
+            // Box1 详述
+            Box1.setLength(3.0);
+            Box1.setBreadth(4.0);
+            Box1.setHeight(5.0);
+
+            // Box2 详述
+            Box2.setLength(2.0);
+            Box2.setBreadth(3.0);
+            Box2.setHeight(4.0);
+
+            // Box1 的体积
+            volume = Box1.getVolume();
+            Console.WriteLine("Box1 的体积： {0}", volume);
+
+            // Box2 的体积
+            volume = Box2.getVolume();
+            Console.WriteLine("Box2 的体积： {0}", volume);
+
+            // 把两个对象相加
+            Box3 = Box1 + Box2;
+
+            // Box3 的体积
+            volume = Box3.getVolume();
+            Console.WriteLine("Box3 的体积： {0}", volume);
+            Console.WriteLine("{0}*{1}*{2}={3}",5,7,9,315);
+            Console.ReadKey();
+        }
+    }
+}
